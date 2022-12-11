6f712fa2da3f75d39a702524cb8bf83d7bfb5c62
Py0000 <pylim8266@gmail.com>
2022-12-12 01:39:32 +0800

    Update

--- Source Code/core/src/com/runningdude/Utilities.java
+++ Source Code/core/src/com/runningdude/Utilities.java
@@ -15,19 +15,14 @@ public abstract class Utilities {
 
     // Involves with generating the diamond
     public static final int DIAMOND_FREQUENCY = 100;
-    public static final int DIAMOND_FACTOR = 5;
+    public static final int DIAMOND_FACTOR = 10;
     public static final int DEFAULT_DIAMOND_COUNT = 0;
 
     // Involves with generating the toxin
     public static final int TOXIN_FREQUENCY = 225;
-    public static final int TOXIN_FACTOR = 9;
+    public static final int TOXIN_FACTOR = 15;
     public static final int DEFAULT_TOXIN_COUNT = 0;
 
     // Involves with scoreboard
     public static final int DEFAULT_SCORE = 0;
-
-    // Involves with Waiting Page
-    public static final String WORD_1 = "TOUCH (ANYWHERE)";
-    public static final String WORD_2 = "THE SCREEN";
-    public static final String WORD_3 = "TO START";
 }
