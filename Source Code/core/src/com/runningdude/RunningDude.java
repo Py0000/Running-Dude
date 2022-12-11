6838ed6e185b696de15d15ff80fbae9d0ece6a4c
Py0000 <pylim8266@gmail.com>
2022-12-11 12:53:49 +0800

    Change app orientation to portrait

--- Source Code/core/src/com/runningdude/RunningDude.java
+++ Source Code/core/src/com/runningdude/RunningDude.java
@@ -8,21 +8,29 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class RunningDude extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	// Called when the app is opened for the first time
 	@Override
 	public void create () {
+		// Used to draw everything to the screen (visually)
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
+	// Runs over and over again until the app is done
 	@Override
 	public void render () {
+		// Make background colour
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
+
+		// Draw the image to the screen
 		batch.draw(img, 0, 0);
+
 		batch.end();
 	}
-	
+
+	// Called when the app is done running
 	@Override
 	public void dispose () {
 		batch.dispose();
