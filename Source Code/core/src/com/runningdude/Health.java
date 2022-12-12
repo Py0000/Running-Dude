e40391ea8a874121c8d3c0456720b26c613eec60
Py0000 <pylim8266@gmail.com>
2022-12-13 00:23:52 +0800

    Add health system

---
+++ Source Code/core/src/com/runningdude/Health.java
@@ -0,0 +1,50 @@
+package com.runningdude;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Health {
+    private static final String FULL_HEALTH = "full-health.png";
+    private static final String ACCEPTABLE_HEALTH = "acceptable-health.png";
+    private static final String DANGER_HEALTH = "danger-health.png";
+
+    Texture fullHealth;
+    Texture accpetableHealth;
+    Texture dangerHealth;
+
+    public Health() {
+        fullHealth = new Texture(FULL_HEALTH);
+        accpetableHealth = new Texture(ACCEPTABLE_HEALTH);
+        dangerHealth = new Texture(DANGER_HEALTH);
+    }
+
+    public void showHealthBar(SpriteBatch batch, int healthState, int gameWidth, int gameHeight) {
+        if (healthState == Utilities.HEALTH_FULL) {
+            batch.draw(fullHealth, gameWidth - 400, gameHeight - 150);
+        }
+
+        else if (healthState == Utilities.HEALTH_ACCEPTABLE) {
+            batch.draw(accpetableHealth, gameWidth - 400, gameHeight - 150);
+        }
+
+        else if (healthState == Utilities.HEALTH_DANGER) {
+            batch.draw(dangerHealth, gameWidth - 400, gameHeight - 150);
+        }
+    }
+
+    public int updateHealth(int currentHealth, SpriteBatch batch, int gameWidth, int gameHeight) {
+        if (currentHealth == Utilities.HEALTH_FULL) {
+            batch.draw(accpetableHealth, gameWidth - 400, gameHeight - 150);
+            return Utilities.HEALTH_ACCEPTABLE;
+        }
+
+        if (currentHealth == Utilities.HEALTH_ACCEPTABLE) {
+            batch.draw(dangerHealth, gameWidth - 400, gameHeight - 150);
+            return Utilities.HEALTH_DANGER;
+        }
+
+        // should not reach here
+        batch.draw(dangerHealth, gameWidth - 400, gameHeight - 150);
+        return Utilities.HEALTH_DANGER;
+    }
+}
